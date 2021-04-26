9eb8c5005569b36ab0bc54d71fe8457278b60cac
Lizandro <lizandrof@gmail.com>
2021-04-26 10:14:54 +0200

    build apk

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.4:3333',
+    //baseURL: 'http://192.168.1.4:3333',
+    baseURL: 'https://my-json-server.typicode.com/lizandrof/plantmanager'
 });
 
 export default api;
